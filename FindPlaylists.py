aa602fbd370cdb3f60094b1b44be1cd732cb7176
tacosico <114280693+tacosico@users.noreply.github.com>
2022-12-09 01:57:32 -0800

    Update FindPlaylists.py

--- FindPlaylists.py
+++ FindPlaylists.py
@@ -1,57 +1,76 @@
-import random # idk if it's needed but just in case
 import pandas as pd
         
     
-class FindPlaylists(Playlists):
+class FindPlaylists():
 '''
 Class that finds playlists from the Million Playlists Spotify dataset that contains user input(s)
 '''
-    def __init__(self):
+    def __init__(self, data):
     '''
-    Prompts user to input varaiables used in the rest of the function. Asks user for input for a song and the optional artist and album of theirs.
-    Arg:
-        none
+    Prompts user to input varaiables used in the rest of the function. Asks user for input for an artist, a song, and album of theirs. 
+    At least one input is required.
+    Args:
+        data: instance of Million Playlist dataset as a csv file
     Returns:
         none
     '''        
+        self.data = data
+        
+        # ask user for inputs that will be used in other functions
         print("Type in the information below, and hit enter. You can leave blank if you need to.")
-        self.artist = input("Track Name : ")
-        self.track = input("Artist Name : ")
+        self.artist = input("Artist Name : ")
+        self.track = input("Track Name : ")
         self.album = input("Album Name : ")
         
+        if self.artist =='' and self.track =='' and self.album =='':
+                raise ValueError("No input entered.")
+        
         # variables that will be used in iteration and displaying results
-        self.maximum = -1 # the highest score
+        self.high_score = -1 
         self.playlist_index = -1
+        
+        
+    def query(self):
+    '''
+    Create a list of nonempty input variables.
+    Args:
+        none
+    Returns:
+        querylist: a list of filled user inputs
+    '''
+        inputs = [self.artist, self.track, self.album]
+        
+        querylist = []
+        querylist.append(inputs[i]) for i in range(len(inputs)) if inputs[i] != ''
+        
+        return querylist
+        
 
     def __str__(self):
     '''
+    Print condition of findRec function based on user input.
     Args:
         none
     Returns:
         mystring: string dependent on user input. "Finding playlists that contains {condition depending on user input}"
     '''
-        if self.artist == "" and self.track == "" and self.album == "":
-            raise ValueError("No input found.")
-        elif self.track == "" and self.album == "":
-            mystring = "Finding playlists that contains songs by " + self.artist "."
-        elif self.album == "":
-            mystring = "Finding playlist that contains " + self.track + " by " + self.artist + "."
-        else
-            mystring = "Finding playlists that contains " + self.track + " by " + self.artist + " from the album " + self.album "."
+        q = self.querylist()
+        s = ', '
+        s = s.join(q)
+        
+        mystring = "Finding playlists that contain music from " + s
         
         return mystring
                 
         
     def findRecs(self):
     '''
-    Iterates through a random slice in Million Playlists dataset and gives a score to each playlist based on how many 
+    Iterates through a random slice in the Million Playlists dataset and gives a score to each playlist based on how many 
     times an artist, track, and/or album is present in a playlist.
-    
     Args:
         self: the variables initialized in the __init__ function
     Returns:
         
-    
     '''
         # Main Code for recommendation
         # Select the tracks column in our data for all playlists
@@ -80,11 +99,11 @@ Class that finds playlists from the Million Playlists Spotify dataset that conta
             average_score = ((artist_score + track_score + album_score)/3)*100
             
             # create new score
-            if(average_score > maximum):
-                self.maximum = average_score
+            if(average_score > high_score):
+                self.high_score = average_score
                 self.playlist_index = i
                 
-        return artist_score, track_score, album_score
+        return self.high_score, self.playlist_index
                 
     def showResults(self):
     ''' 
@@ -94,21 +113,13 @@ Class that finds playlists from the Million Playlists Spotify dataset that conta
     Returns:
         none 
     '''
-        # And finally recommend the playlist from that json
-        playlist_name = df.loc[playlist_index]['name']
-        query_string = []
-        if(artist_name != ""):
-            query_string.append(artist_name)
-        if(track_name != ""):
-            query_string.append(track_name)
-        if(album_name != ""):
-            query_string.append(album_name)
+        playlist_name = df.loc[self.playlist_index]['name']
             
         # Recommend and print the output
         if(maximum > 0):
             print("😃😃 You should listen to the playlist called "+str(playlist_name)+".")
-            print("♫ 🎸It has the closest match to "+str(query_string)+" with a score of "+str(maximum)+".")
-            results = pd.DataFrame(all_tracks[playlist_index])
+            print("♫ 🎸It has the closest match to "+str(self.query())+" with a score of "+str(self.high_score)+".")
+            results = pd.DataFrame(all_tracks[self.playlist_index])
             display(results[['artist_name', 'track_name','album_name']])
         else:
             print("😭😭😭 Sorry, no recommendation found.")
